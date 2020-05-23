2cc9875a74fc0190747e40c9c583d10870fd7d07
Kristian Tot <kristian.tot@pbssolutions.net>
2020-05-23 19:13:08 +0200

    Added dumpingProperty.cs model

---
+++ Cache Memory/ModelsAndProps/Dumping buffer/DumpingProperty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsAndProps.Dumping_buffer
+{
+    public class DumpingProperty
+    {
+        private Codes code;
+        private float dumpingValue;
+
+        public Codes Code { get => code; set => code = value; }
+        public float DumpingValue { get => dumpingValue; set => dumpingValue = value; }
+
+        public DumpingProperty()
+        {
+
+        }
+
+        public DumpingProperty(Codes code, float dumpValue)
+        {
+            this.code = code;
+            this.dumpingValue = dumpValue;
+        }
+    }
+}
