c427783f0178f0a2227a52a040ac7d5cc43738d5
Dusan Mazic <dusanmazic123@gmail.com>
2020-05-23 19:38:02 +0200

    Added historical description

---
+++ Cache Memory/ModelsAndProps/Historical/HistoricalDescription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsAndProps.Historical
+{
+    public class HistoricalDescription
+    {
+        private int id;
+        private int dataset;
+        private HashSet<HistoricalProperty> historicalProperties = new HashSet<HistoricalProperty>();
+
+        public HistoricalDescription()
+        {
+
+        }
+
+        public HistoricalDescription(int id, int dataset)
+        {
+            this.Id = id;
+            this.dataset = dataset;
+        }
+
+        public int Dataset { get => dataset; set => dataset = value; }
+        public HashSet<HistoricalProperty> HistoricalProperties { get => historicalProperties; set => historicalProperties = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
