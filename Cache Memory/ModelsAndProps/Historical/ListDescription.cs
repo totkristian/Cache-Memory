54fa8b662b17e23cbd184ea5803000089bc030fc
Dusan Mazic <dusanmazic123@gmail.com>
2020-05-23 19:41:24 +0200

    Added list description

---
+++ Cache Memory/ModelsAndProps/Historical/ListDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsAndProps.Historical
+{
+    public class ListDescription
+    {
+        HashSet<HistoricalDescription> historicalDescriptions = new HashSet<HistoricalDescription>();
+
+        public HashSet<HistoricalDescription> HistoricalDescriptions { get => historicalDescriptions; set => historicalDescriptions = value; }
+
+        public ListDescription()
+        {
+
+        }
+    }
+}
