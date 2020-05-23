8fd6f1a6a2419b15dc0247db69a2517c7ba9adf3
Dusan Mazic <dusanmazic123@gmail.com>
2020-05-23 19:26:48 +0200

    Added historical property

---
+++ Cache Memory/ModelsAndProps/Historical/Historical.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsAndProps.Historical
+{
+    public class Historical
+    {
+        private Codes codes;
+        private float historicalValue;
+        private DateTime time;
+
+        public Codes Codes { get => codes; set => codes = value; }
+        public float HistoricalValue { get => historicalValue; set => historicalValue = value; }
+        public DateTime Time { get => time; set => time = value; }
+
+
+        public Historical()
+        {
+
+        }
+
+        public Historical(Codes codes, float value)
+        {
+            this.codes = codes;
+            this.historicalValue = value;
+        }
+    }
+}
