08e5a281e3a9cc666430cf53827867511d23dd73
Kristian Tot <kristian.tot@pbssolutions.net>
2020-05-25 07:02:33 +0200

    added ValueStructure.cs model

---
+++ Cache Memory/ModelsAndProps/ValueStructure/Value.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsAndProps.ValueStructure
+{
+    public class Value
+    {
+        private DateTime timestamp;
+        private int geographicalLocationId;
+        private double consumption;
+
+        public DateTime Timestamp { get => timestamp; set => timestamp = value; }
+        public int GeographicalLocationId { get => geographicalLocationId; set => geographicalLocationId = value; }
+        public double Consumption { get => consumption; set => consumption = value; }
+
+        public Value()
+        {
+
+        }
+        public Value(DateTime timestamp, int geoId,double usage)
+        {
+            this.timestamp = timestamp;
+            this.geographicalLocationId = geoId;
+            this.consumption = usage;
+        }
+       
+    }
+}
