0b6eedf46921946df26fb78e6985b8fed7666302
Kristian Tot <kristian.tot@pbssolutions.net>
2020-06-01 19:40:48 +0200

    added listDescription migration

---
+++ Cache Memory/Cache Memory/Migrations/202006011740276_CreateListDescription.cs
@@ -0,0 +1,59 @@
+﻿namespace Cache_Memory.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateListDescription : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ListDescriptions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.HistoricalDescriptions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Dataset = c.Int(nullable: false),
+                        ListDescription_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ListDescriptions", t => t.ListDescription_Id)
+                .Index(t => t.ListDescription_Id);
+            
+            CreateTable(
+                "dbo.HistoricalProperties",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Codes = c.Int(nullable: false),
+                        HistoricalValue_Timestamp = c.DateTime(nullable: false),
+                        HistoricalValue_GeographicalLocationId = c.Int(nullable: false),
+                        HistoricalValue_Consumption = c.Double(nullable: false),
+                        Time = c.DateTime(nullable: false),
+                        HistoricalDescription_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.HistoricalDescriptions", t => t.HistoricalDescription_Id)
+                .Index(t => t.HistoricalDescription_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.HistoricalDescriptions", "ListDescription_Id", "dbo.ListDescriptions");
+            DropForeignKey("dbo.HistoricalProperties", "HistoricalDescription_Id", "dbo.HistoricalDescriptions");
+            DropIndex("dbo.HistoricalProperties", new[] { "HistoricalDescription_Id" });
+            DropIndex("dbo.HistoricalDescriptions", new[] { "ListDescription_Id" });
+            DropTable("dbo.HistoricalProperties");
+            DropTable("dbo.HistoricalDescriptions");
+            DropTable("dbo.ListDescriptions");
+        }
+    }
+}
