35c2d61fb91f280837520a368580298b9a0efccd
Kristian Tot <kristian.tot@pbssolutions.net>
2020-06-01 19:33:24 +0200

    added database class and entity framework

---
+++ Cache Memory/Cache Memory/Database/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelsAndProps;
+
+
+namespace Cache_Memory.Database
+{
+    public class Database : DbContext
+    {
+        public Database() :base("")
+        {
+
+        }
+        
+    }
+}
