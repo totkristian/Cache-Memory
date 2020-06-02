fd63196edbe57e0ca42af8af74f9916e97c4bb9a
Dusan Mazic <dusanmazic123@gmail.com>
2020-06-02 16:34:00 +0200

    Added historical component and maded singltone instance

--- Cache Memory/HistoricalComponent/Historical.cs
+++ Cache Memory/HistoricalComponent/Historical.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,27 @@ namespace HistoricalComponent
 {
     public class Historical
     {
+        private static Historical instance;
+        private static object syncLock = new object();
+
+        public Historical()
+        {
+
+        }
+
+        public static Historical GetInstance()
+        {
+            lock (syncLock)
+            {
+                if (instance == null)
+                {
+                    instance = new Historical();
+                }
+            }
+
+            return instance;
+        }
+
+
     }
 }
