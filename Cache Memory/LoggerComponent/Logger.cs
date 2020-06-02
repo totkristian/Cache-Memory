58b2c276dae5d35cc106b130682e903651257a46
Kristian Tot <kristian.tot@pbssolutions.net>
2020-06-02 12:19:35 +0200

    added writeLog and readLog methods

--- Cache Memory/LoggerComponent/Logger.cs
+++ Cache Memory/LoggerComponent/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,39 @@ namespace LoggerComponent
 {
     public class Logger
     {
+        public static void WriteLog(string msg, string className, string method)
+        {
+            if(String.IsNullOrWhiteSpace(msg) || String.IsNullOrWhiteSpace(className) || String.IsNullOrWhiteSpace(method))
+            {
+                throw new ArgumentException("Parameters cannot be empty,null or whitespace!");
+            }
+
+            using (StreamWriter sw = new StreamWriter("log.txt"))
+            {
+                sw.WriteLine("---Log Entry:");
+                sw.WriteLine($"---{DateTime.Now.ToString()}");
+                sw.WriteLine($"\tClass name: {className}");
+                sw.WriteLine($"\tMethod name: {method}");
+                sw.WriteLine($"\tMessage: {msg}");
+                sw.WriteLine("-----------------------------------------");
+            }
+        }
+
+        public static List<string> ReadLog()
+        {
+            List<string> log = new List<string>();
+            using (StreamReader sr = new StreamReader("log.txt"))
+            {
+                if (sr == null)
+                    throw new ArgumentNullException("Log reader failed to open file!");
+
+                string line = "";
+                while ((line = sr.ReadLine()) != null) {    
+                    log.Add(sr.ReadLine());
+                }
+            }
+            return log;
+        }
 
     }
 }
