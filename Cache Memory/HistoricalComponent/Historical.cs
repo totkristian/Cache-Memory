e25e1287cb7155b6a2349b868a21f94bd78e2fc5
Dusan Mazic <dusanmazic123@gmail.com>
2020-06-08 17:13:55 +0200

    Added GetChangesForConsumerOrSource function

--- Cache Memory/HistoricalComponent/Historical.cs
+++ Cache Memory/HistoricalComponent/Historical.cs
@@ -134,7 +134,18 @@ namespace HistoricalComponent
 
         private List<HistoricalProperty> GetChangesForConsumerOrSource(Codes code)
         {
-            throw new NotImplementedException();
+            List<HistoricalProperty> ret = new List<HistoricalProperty>();
+            foreach (HistoricalDescription hd in (List<HistoricalDescription>)listDescription.Where(x => x.Id == 4).Select(x => x.HistoricalDescriptions))
+            {
+                foreach (HistoricalProperty hp in hd.HistoricalProperties)
+                {
+                    if (code.Equals(hp.Codes))
+                    {
+                        ret.Add(hp);
+                    }
+                }
+            }
+            return ret;
         }
 
         private List<HistoricalProperty> GetChangesForSinglenodeOrMultiplenode(Codes code)
