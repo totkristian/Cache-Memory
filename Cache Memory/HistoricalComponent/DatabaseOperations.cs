c283dfc71b9a1dddf7e37988783625bddf318014
Kristian Tot <kristian.tot@pbssolutions.net>
2020-06-14 17:46:53 +0200

    changed the readListDescription function (added it to the other class)

--- Cache Memory/HistoricalComponent/DatabaseOperations.cs
+++ Cache Memory/HistoricalComponent/DatabaseOperations.cs
@@ -14,9 +14,26 @@ namespace HistoricalComponent
 
         public void AddHistoricalDescription(HistoricalDescription hd, int dataset)
         {
+            //call logger
             ListDescription list1 = database.ListDescriptions.Where(x => x.Id == dataset).FirstOrDefault();
             list1.HistoricalDescriptions.Add(hd);
             database.SaveChanges();
+        }
+
+        public ListDescription ReadListDescription(int dataset)
+        {
+            ListDescription ld = new ListDescription();
+            List<HistoricalDescription> list = database.HistoricalDescriptions.Where(x => x.ListDescriptionId == dataset).ToList();
+            ld.HistoricalDescriptions = list;
+
+            for (int i = 0; i < ld.HistoricalDescriptions.Count; i++)
+            {
+                int id = ld.HistoricalDescriptions[i].Id;
+                List<HistoricalProperty> hpList = database.HistoricalProperties.Where(x => x.HistoricalDescriptionId == id).ToList();
+                ld.HistoricalDescriptions[i].HistoricalProperties = hpList;
+            }
+
+            return ld;
         }
     }
 }
--- Cache Memory/HistoricalComponent/Historical.cs
+++ Cache Memory/HistoricalComponent/Historical.cs
@@ -135,18 +135,7 @@ namespace HistoricalComponent
 
         public ListDescription ReadOneLDFromDB(int dataset)
         {
-            ListDescription ld = new ListDescription();
-            List<HistoricalDescription> list = database.HistoricalDescriptions.Where(x => x.ListDescriptionId == dataset).ToList();
-            ld.HistoricalDescriptions = list;
-
-            for (int i = 0; i < ld.HistoricalDescriptions.Count; i++)
-            {
-                int id = ld.HistoricalDescriptions[i].Id;
-                List<HistoricalProperty> hpList = database.HistoricalProperties.Where(x => x.HistoricalDescriptionId == id).ToList();
-                ld.HistoricalDescriptions[i].HistoricalProperties = hpList;
-            }
-
-            return ld;
+            return databaseOperations.ReadListDescription(dataset);
         }
 
         public List<HistoricalProperty> GetHistoricalProperties()
