ea816d92b5fb45f3d0f098c0c096cc1e263629cf
Kristian Tot <kristian.tot@pbssolutions.net>
2020-06-15 17:30:34 +0200

    added loggerTest

---
+++ Cache Memory/LoggerComponentTest/LoggerTest.cs
@@ -0,0 +1,46 @@
+﻿using LoggerComponent;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerComponentTest
+{
+    [TestFixture]
+    public class LoggerTest
+    {
+        [Test]
+        [TestCase("message","className", "method")]
+        public void GoodLog(string msg, string className, string method) {
+            Assert.DoesNotThrow(() =>
+            {
+                Logger.WriteLog(msg, className, method);
+            });
+        }
+        [Test]
+        [TestCase(null,"className","method")]
+        [TestCase(null,null,"method")]
+        [TestCase(null,null,null)]
+        [TestCase(null,"className",null)]
+        [TestCase(null,null,"method")]
+        [TestCase("Message",null,"method")]
+        [TestCase("Message",null,null)]
+        public void ArgumentExceptionLog(string msg, string className, string method)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Logger.WriteLog(msg, className, method);
+            });
+        }
+        [Test]
+        public void GoodReadLog()
+        {
+            Assert.DoesNotThrow(()=>
+            {
+                Logger.ReadLog();
+            });
+        }
+    }
+}
