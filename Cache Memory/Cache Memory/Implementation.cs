950dc0514df2e31e1ad11dabee7f0e06c0425453
Dusan Mazic <dusanmazic123@gmail.com>
2020-06-15 11:53:41 +0200

    Nothing

--- Cache Memory/Cache Memory/Implementation.cs
+++ Cache Memory/Cache Memory/Implementation.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using ReaderComponent;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WriterComponent;
 
 namespace Cache_Memory
 {
-    class Implementation
+    public class Implementation
     {
+        Writer w = new Writer();
+        Reader r = new Reader();
+        public void MenuChoise()
+        {
+            Console.WriteLine("1. Write data automatically (dumpingBuffer)");
+            Console.WriteLine("2. Write data manually (direct to historical)");
+            Console.WriteLine("3. Read data");
+        }
     }
 }
