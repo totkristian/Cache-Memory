481c958edf3387741251b165dafdcb5a7910bac4
Dusan Mazic <dusanmazic123@gmail.com>
2020-06-15 12:16:57 +0200

    Added meni

--- Cache Memory/Cache Memory/Implementation.cs
+++ Cache Memory/Cache Memory/Implementation.cs
@@ -19,5 +19,64 @@ namespace Cache_Memory
             Console.WriteLine("3. Read data");
             Console.WriteLine("4. Exit app");
         }
+        public int MenuChoiseInput()
+        {
+            int number = 0;
+            bool isOk = false;
+            while (!isOk)
+            {
+                MenuChoise();
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    if (number >= 1 && number <= 4)
+                    {
+                        isOk = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Choise does not exist!");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nIt needs to be a number!\n");
+                }
+            }
+            return number;
+        }
+
+
+        public void ReadData()
+        {
+            ConsoleKeyInfo cki;
+            do
+            {
+                r.Meni();
+                Console.Write("\nPress 'Escape' to exit reader");
+                cki = Console.ReadKey();
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+
+        public void SendToDumpingBuffer()
+        {
+            Console.Write("\nPress any key to exit dumping buffer");
+            while (!Console.KeyAvailable)
+            {
+                w.SendToDumpingBuffer();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
