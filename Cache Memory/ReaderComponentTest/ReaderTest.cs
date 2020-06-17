e062b8509a44f75a07725d7acab907bb9b903e4c
Dusan Mazic <dusanmazic123@gmail.com>
2020-06-17 19:34:39 +0200

    Added ReaderComponentTest

---
+++ Cache Memory/ReaderComponentTest/ReaderTest.cs
@@ -0,0 +1,29 @@
+﻿using ModelsAndProps.ValueStructure;
+using Moq;
+using NUnit.Framework;
+using ReaderComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderComponentTest
+{
+    [TestFixture]
+    public class ReaderTest
+    {
+        Mock<Reader> readerMock;
+        Mock<Value> valueMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            readerMock = new Mock<Reader>();
+            valueMock = new Mock<Value>();
+            valueMock.Object.Consumption = 10.0;
+            valueMock.Object.GeographicalLocationId = Guid.NewGuid().ToString();
+            valueMock.Object.Timestamp = DateTime.Now;
+        }
+    }
+}
